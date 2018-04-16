ea45dc2115280254c61046abfef7880373ac1b4b
Mariama Jaiteh <mjaiteh@smith.edu>
2018-04-15 23:32:13 -0400

    Home page change

--- react-firebase-authentication/src/components/Home/index.js
+++ react-firebase-authentication/src/components/Home/index.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-
-import withAuthorization from '../Session/withAuthorization';
-import { db } from '../../firebase';
+import { Link } from 'react-router-dom';
+import * as routes from '../../constants/routes';
 
 class HomePage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      users: {}
-    };
-  }
-
-  componentDidMount() {
-    db.onceGetUsers().then(snapshot =>
-      this.setState(() => ({ users: snapshot.val() }))
-    );
-  }
 
   render() {
-    const { users } = this.state;
 
     return (
       <div>
-        <button> Click </button>
+        <button><Link to={routes.WriteEntry}>Click</Link>
+        </button>
 
       </div>
     );
@@ -32,6 +18,5 @@ class HomePage extends Component {
 
 
 
-const authCondition = (authUser) => !!authUser;
 
-export default withAuthorization(authCondition)(HomePage);
+export default HomePage;
